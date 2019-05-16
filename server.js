6d34018556b7923f1693e78b916f864e40e23e84
Sonja Rasmussen <sonja.rasmussen@gmail.com>
2019-05-16 11:42:42 -0700

    updated server.js to work with proxy

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, 'build')));
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
