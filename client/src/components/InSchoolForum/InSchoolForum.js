b3aa1660ce31db09534b83a5c27f90ac29a16adb
Julie Mathews <juliem.forever@gmail.com>
2019-05-24 20:39:32 -0700

    a tags removed, added navbar header footer, image tags corrected

--- client/src/components/InSchoolForum/InSchoolForum.js
+++ client/src/components/InSchoolForum/InSchoolForum.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../css/style.css';
+import Navbar from '../Navbar/Navbar';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 // Header message = { this.state.info }
 
 const InSchoolForum = (props) => {
   return (
+    <div>
+    <Navbar />
+    <Header />
     <section class="banner_area">
       <div class="banner_inner d-flex align-items-center">
         <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
@@ -25,18 +31,18 @@ const InSchoolForum = (props) => {
                     <div class="blog_info text-right">
                       <ul class="blog_meta list">
                         <div class="thumb">
-                          <img src="img/blog/c5.jpg" alt="">
+                          <img src={ require('../../img/blog/c5.jpg') } alt="c5" />
                         </div>
-                        <li><a href="#">Bob<i class="lnr lnr-user"></i></a></li>
-                        <li><a href="#">3 May, 2019<i class="lnr lnr-calendar-full"></i></a></li>
-                        <li><a href="#">05 Comments<i class="lnr lnr-bubble"></i></a></li>
+                        <li>Bob<i class="lnr lnr-user"></i></li>
+                        <li>3 May, 2019<i class="lnr lnr-calendar-full"></i></li>
+                        <li>05 Comments<i class="lnr lnr-bubble"></i></li>
                       </ul>
                     </div>
                   </div>
                 <div class="col-md-9">
                   <div class="blog_post">
                     <div class="blog_details">
-                      <a href="singlePost.html"><h2>What is an IEP?</h2></a>
+                      <a href="singlePost.html"><h2>What is an IEP?</h2>
                       <p>Donec ultrices tincidunt arcu non sodales neque sodales ut etiam. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Pellentesque id nibh tortor id aliquet lectus proin nibh.</p>
                     </div>
                   </div>
@@ -47,18 +53,18 @@ const InSchoolForum = (props) => {
                   <div class="blog_info text-right">
                     <ul class="blog_meta list">
                       <div class="thumb">
-                        <img src="img/blog/c1.jpg" alt="">
+                        <img src={ require('../../img/blog/c1.jpg') } alt="c1" />
                       </div>
-                      <li><a href="#">Emily<i class="lnr lnr-user"></i></a></li>
-                      <li><a href="#">3 May, 2019<i class="lnr lnr-calendar-full"></i></a></li>
-                      <li><a href="#">05 Comments<i class="lnr lnr-bubble"></i></a></li>
+                      <li>Emily<i class="lnr lnr-user"></i></li>
+                      <li>3 May, 2019<i class="lnr lnr-calendar-full"></i></li>
+                      <li>05 Comments<i class="lnr lnr-bubble"></i></li>
                     </ul>
                   </div>
                 </div>
                 <div class="col-md-9">
                   <div class="blog_post">
                     <div class="blog_details">
-                      <a href="singlePost.html"><h2>What do I pack my picky eater for school?</h2></a>
+                      <a href="singlePost.html"><h2>What do I pack my picky eater for school?</h2>
                       <p>Imperdiet proin fermentum leo vel orci porta non pulvinar. Amet dictum sit amet justo donec enim diam vulputate ut.</p>
                     </div>
                   </div>
@@ -69,18 +75,18 @@ const InSchoolForum = (props) => {
                   <div class="blog_info text-right">
                   <ul class="blog_meta list">
                     <div class="thumb">
-                      <img src="img/blog/c2.jpg" alt="">
+                      <img src={ require('../../img/blog/c2.jpg') } alt="c2" />
                     </div>
-                    <li><a href="#">Henry<i class="lnr lnr-user"></i></a></li>
-                    <li><a href="#">2 May, 2019<i class="lnr lnr-calendar-full"></i></a></li>
-                    <li><a href="#">05 Comments<i class="lnr lnr-bubble"></i></a></li>
+                    <li>Henry<i class="lnr lnr-user"></i></li>
+                    <li>2 May, 2019<i class="lnr lnr-calendar-full"></i></li>
+                    <li>05 Comments<i class="lnr lnr-bubble"></i></li>
                   </ul>
                 </div>
               </div>
               <div class="col-md-9">
                 <div class="blog_post">
                   <div class="blog_details">
-                    <a href="singlePost.html"><h2>No one will play with my child at recess.</h2></a>
+                    <a href="singlePost.html"><h2>No one will play with my child at recess.</h2>
                     <p>Sed adipiscing diam donec adipiscing tristique risus nec. Risus viverra adipiscing at in tellus integer feugiat scelerisque varius.</p>
                   </div>
                 </div>
@@ -91,18 +97,18 @@ const InSchoolForum = (props) => {
                 <div class="blog_info text-right">
                   <ul class="blog_meta list">
                     <div class="thumb">
-                      <img src="img/blog/c3.jpg" alt="">
+                      <img src={ require('../../img/blog/c3.jpg') } alt="c3" />
                     </div>
-                    <li><a href="#">Annie<i class="lnr lnr-user"></i></a></li>
-                    <li><a href="#">2 May, 2019<i class="lnr lnr-calendar-full"></i></a></li>
-                    <li><a href="#">05 Comments<i class="lnr lnr-bubble"></i></a></li>
+                    <li>Annie<i class="lnr lnr-user"></i></li>
+                    <li>2 May, 2019<i class="lnr lnr-calendar-full"></i></li>
+                    <li>05 Comments<i class="lnr lnr-bubble"></i></li>
                   </ul>
                 </div>
               </div>
               <div class="col-md-9">
                 <div class="blog_post">
                   <div class="blog_details">
-                    <a href="singlePost.html"><h2>HELP! There's a bully on the school bus.</h2></a>
+                    <a href="singlePost.html"><h2>HELP! There's a bully on the school bus.</h2>
                     <p>Aliquet bibendum enim facilisis gravida neque convallis a. Habitant morbi tristique senectus et netus. Lacus vestibulum sed arcu non odio euismod lacinia at.</p>
                   </div>
                 </div>
@@ -113,48 +119,49 @@ const InSchoolForum = (props) => {
                 <div class="blog_info text-right">
                   <ul class="blog_meta list">
                     <div class="thumb">
-                      <img src="img/blog/c1.jpg" alt="">
+                      <img src={ require('../../img/blog/c1.jpg') } alt="c1" />
                     </div>
-                    <li><a href="#">Emily<i class="lnr lnr-user"></i></a></li>
-                    <li><a href="#">1 May, 2019<i class="lnr lnr-calendar-full"></i></a></li>
-                    <li><a href="#">05 Comments<i class="lnr lnr-bubble"></i></a></li>
+                    <li>Emily<i class="lnr lnr-user"></i></li>
+                    <li>1 May, 2019<i class="lnr lnr-calendar-full"></i></li>
+                    <li>05 Comments<i class="lnr lnr-bubble"></i></li>
                   </ul>
                 </div>
               </div>
               <div class="col-md-9">
                 <div class="blog_post">
                   <div class="blog_details">
-                    <a href="singlePost.html"><h2>Inclusion or exclusion?</h2></a>
+                    <a href="singlePost.html"><h2>Inclusion or exclusion?</h2>
                     <p>Metus aliquam eleifend mi. Sed adipiscing diam donec adipiscing tristique risus nec. </p>
                   </div>
                 </div>
               </div>
             </article>
             <nav class="blog-pagination justify-content-center d-flex">
-              <ul class="pagination">
-                <li class="page-item">
-                  <a href="#" class="page-link" aria-label="Previous">
-                    <span aria-hidden="true">
-                      <span class="lnr lnr-chevron-left"></span>
-                    </span>
-                  </a>
-                </li>
-                <li class="page-item active"><a href="#" class="page-link">01</a></li>
-                <li class="page-item"><a href="#" class="page-link">02</a></li>
-                <li class="page-item"><a href="#" class="page-link">03</a></li>
-                <li class="page-item"><a href="#" class="page-link">04</a></li>
-                <li class="page-item"><a href="#" class="page-link">09</a></li>
-                <li class="page-item">
-                  <a href="#" class="page-link" aria-label="Next">
-                    <span aria-hidden="true">
-                      <span class="lnr lnr-chevron-right"></span>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
+                <ul class="pagination">
+                  <li class="page-item">
+                    <div class="page-link" aria-label="Previous">
+                      <span aria-hidden="true">
+                        <img src={ require('../../img/prev.png') } alt="prev" />
+                      </span>
+                    </div>
+                  </li>
+                  <li class="page-item active page-link">01</li>
+
+                  <li class="page-item page-link">02</li>
+                  <li class="page-item page-link">03</li>
+                  <li class="page-item page-link">04</li>
+
+                  <li class="page-item page-link" aria-label="Next">
+                      <span aria-hidden="true">
+                        <img src={ require('../../img/next.png') } alt="next" />
+                      </span>
+                  </li>
+                </ul>
+              </nav>
           </div>
         </div>
+{/*
+        !!!!! Searchbar commented out until we need it !!!!!
         <div class="col-lg-4">
           <div class="blog_right_sidebar">
             <aside class="single_sidebar_widget search_widget">
@@ -168,8 +175,11 @@ const InSchoolForum = (props) => {
             </aside>
           </div>
         </div>
+*/}
       </div>
     </section>
+    <Footer />
+    </div>
   )
 };
 
