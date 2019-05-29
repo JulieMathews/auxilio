2f2a2f51ffb4f602c66f17d11c8b81de797cb179
SonjaRasmussen <sonja.rasmussen@gmail.com>
2019-05-28 21:57:26 -0700

    update readme

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
 <BrowserRouter>
 <App />
-</BrowserRouter>, 
+</BrowserRouter>,
 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
